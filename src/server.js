03edaa3f68c6b73bd417b0e3a9a685f5adbfc56f
Tim Bobb <31445004+timbobb@users.noreply.github.com>
2021-12-20 21:16:37 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,17 @@
 const helpers = require('handlebars-helpers')();
 var server = require('express')();
 
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
 require('./config/security')(server);
 require('./config/handlebars')(server);
 require('./config/bodyparser')(server);
 require('./routes/routes')(server);
 
-server.listen(process.env.PORT ? process.env.PORT : 3000, function () {
-    console.log("Node JS app is running at http://localhost:" + (process.env.PORT ? process.env.PORT : 3000));
-});
+//server.listen(process.env.PORT ? process.env.PORT : 3000, function () {
+//    console.log("Node JS app is running at http://localhost:" + (process.env.PORT ? process.env.PORT : 3000));
+//});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
