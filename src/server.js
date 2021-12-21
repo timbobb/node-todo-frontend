4e3c90758b1853c91b8660acd39aaf2fb02452bd
Tim Bobb <31445004+timbobb@users.noreply.github.com>
2021-12-20 21:02:11 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@
 const helpers = require('handlebars-helpers')();
 var server = require('express')();
 
+var url = process.env.url
+server.set('port', (process.env.PORT || 3000))
+
 require('./config/security')(server);
 require('./config/handlebars')(server);
 require('./config/bodyparser')(server);
 require('./routes/routes')(server);
 
-server.listen(process.env.PORT ? process.env.PORT : 3000, function () {
-    console.log("Express server listening on port " + (process.env.PORT ? process.env.PORT : 3000));
-});
+
+server.listen(app.get('port'), function() {
+console.log("Node JS app is running at http://localhost:" + server.get('port') +"/bilatechsolutions");
+      })
